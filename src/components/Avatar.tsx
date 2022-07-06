dd76b745eef1a12befbdbfbb9ceee107fe84dafa
Umer Nasir <umer.nas@outlook.com>
2022-07-06 18:02:39 +0500

    Added Styles

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -15,12 +15,38 @@ const Avatar = () => {
   }, [dispatch]);
 
   const charactersList = (
-    <section>
+    <section
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        border: "1px solid red",
+        justifyContent: "center",
+      }}
+    >
       {characters.map((character) => (
-        <article key={character.char_id}>
-          <h3>{character.name}</h3>
-          <img src={character.img} alt={character.name} />
-          <p>{character.nickname}</p>
+        <article
+          style={{
+            width: "150px",
+            height: "180px",
+            margin: "10px",
+            border: "1px solid black",
+            padding: "5px",
+            borderRadius: "5px",
+          }}
+          className="center"
+          key={character.char_id}
+        >
+          <h3 style={{ marginBottom: "10px" }}>{character.name}</h3>
+          <img
+            src={character.img}
+            alt={character.name}
+            width={140}
+            height={120}
+            style={{ objectFit: "fill" }}
+          />
+          <p style={{ marginTop: "10px", fontStyle: "italic" }}>
+            {character.nickname}
+          </p>
         </article>
       ))}
     </section>
